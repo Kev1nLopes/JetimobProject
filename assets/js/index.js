f6592773dfa77dd9448938c1623d1007411d971f
Kev1nLopes <ketisinho14@gmail.com>
2021-11-24 11:55:56 -0300

    new section is almost completed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,35 @@
 let c = (el)=>document.querySelector(el);
 let cs = (el)=>document.querySelectorAll(el);
 
+const debounce = (func, delay) => {
+    let debounceTimer
+    return function() {
+        const context = this
+        const args = arguments
+            clearTimeout(debounceTimer)
+                debounceTimer
+            = setTimeout(() => func.apply(context, args), delay)
+    }
+} 
 
-function createElement(el){
-    return document.createElement(el);
+const target = cs('[data-anime]');
+const animateClass = 'animate';
+
+window.addEventListener('scroll', ()=>{ debounce(animeScroll(), 200)});
+
+
+function animeScroll(){
+    const windowTop = window.scrollY + 50;
+    target.forEach((el)=>{
+        if((windowTop) > el.offsetTop){
+            el.classList.add(animateClass);
+        }else{
+            el.classList.remove(animateClass);
+        }
+    })
 }
 
-function append(ft, el){
-    //ft = father
-    ft.appendChild(el);
-}
+window.onload(animeScroll())
 
 
 //<element>.offsetTop retorna a distancia do elemento ao topo
